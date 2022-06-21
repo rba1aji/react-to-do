50d2ab9ffac28d13b00ff077348c159372506719
rba1aji <rjibalaji@gmail.com>
2022-06-21 14:51:00 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,2 +1,48 @@
 import React from 'react';
-impo
+import logo from './logo.svg';
+import './App.css';
+
+import { useState } from 'react';
+
+function App() {
+  const [task, setTask] = useState('');
+  const [todo, setTodo] = useState([]);
+
+  function UpdateTodo() {
+    setTask('');
+    setTodo((old) => {
+      return [...old, task];
+    });
+  }
+
+  function Input(){
+    return(
+      <>
+      <input value={task} onChange={(event) => setTask(event.target.value)} /> 
+
+      <button type="submit" onClick={UpdateTodo}>
+        set to do
+      </button>
+      </>
+    )
+  }
+
+  function Output() {
+    return (
+      <ul>
+        {todo.map((i) => {
+          return <li>{i}</li>;
+        })}
+      </ul>
+    );
+  }
+
+  return (
+    <>
+      <Input />
+      <Output />
+    </>
+  );
+}
+
+export default App;
