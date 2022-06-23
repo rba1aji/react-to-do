f803477133e7cd3d0bb9e06ee03d090958ff0f3e
Balaji <rjibalaji@gmail.com>
2022-06-23 08:52:26 +0530

    2files modified 1 file deleted

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React, { StrictMode } from 'react';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import { StrictMode } from "react";
+import reportWebVitals from "./reportWebVitals";
+import { sendToVercelAnalytics } from "./vitals";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
